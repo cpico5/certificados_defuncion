a0ea2480aabce887778618fbfe31efeed1604de5
sagrevyum7 <sagrevyum7@users.noreply.github.com>
2020-05-14 01:27:05 -0500

    sube fotos

---
+++ app/src/main/java/com/example/certificadosdefuncion/ImagenAdapter.java
@@ -0,0 +1,79 @@
+package com.example.certificadosdefuncion;
+
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.certificadosdefuncion.model.Imagen;
+
+import java.util.List;
+
+
+public class ImagenAdapter extends RecyclerView.Adapter<ImagenAdapter.ImagenViewHolder> {
+
+    private List<Imagen> listaImagen;
+    private View.OnClickListener mOnItemClickListener;
+    private View.OnClickListener mOnItemClickListenerDelete;
+
+
+    public ImagenAdapter(List<Imagen> listaImagenActa) {
+        this.listaImagen = listaImagenActa;
+    }
+
+    public void setOnItemClickListener(View.OnClickListener itemClickListener) {
+        mOnItemClickListener = itemClickListener;
+    }
+
+    public void setOnItemClickListenerDelete(View.OnClickListener itemClickListener) {
+        mOnItemClickListenerDelete = itemClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ImagenViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list_fotos, parent, false);
+        ImagenViewHolder viewHolder = new ImagenViewHolder(view);
+        return viewHolder;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(@NonNull ImagenViewHolder holder, int position) {
+        //holder.txtNombre.setText(listaImagen.get(position).getPathImagen());
+        position++;
+
+        holder.txtNombre.setText("Imagen " + position);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaImagen.size();
+    }
+
+
+    public class ImagenViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtNombre;
+        ImageButton btnDelete;
+
+        public ImagenViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtNombre =     itemView.findViewById(R.id.idNombre);
+            btnDelete =   (ImageButton)  itemView.findViewById(R.id.btnDelete);
+
+            itemView.setTag(this);
+            itemView.setOnClickListener(mOnItemClickListener);
+
+            btnDelete.setTag(this);
+            btnDelete.setOnClickListener(mOnItemClickListenerDelete);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/certificadosdefuncion/model/Imagen.java
@@ -0,0 +1,79 @@
+package com.example.certificadosdefuncion.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class Imagen implements Serializable {
+
+    private int id;
+    @SerializedName("idCertificado")
+    private int idCertificado;
+    @SerializedName("path_imagen")
+    private String pathImagen;
+    @SerializedName("type")
+    private String tipoImagen;
+    @SerializedName("certificado_id")
+    private int certificadoId;
+    private int enviado;
+
+    public Imagen() {
+    }
+
+    public Imagen(int id, int idCertificado, String pathImagen, String tipoImagen,int certificadoId, int enviado) {
+        this.id = id;
+        this.idCertificado = idCertificado;
+        this.pathImagen = pathImagen;
+        this.tipoImagen = tipoImagen;
+        this.certificadoId = certificadoId;
+        this.enviado = enviado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdCertificado() {
+        return idCertificado;
+    }
+
+    public void setIdCertificado(int idCertificado) {
+        this.idCertificado = idCertificado;
+    }
+
+    public String getPathImagen() {
+        return pathImagen;
+    }
+
+    public void setPathImagen(String pathImagen) {
+        this.pathImagen = pathImagen;
+    }
+
+    public String getTipoImagen() {
+        return tipoImagen;
+    }
+
+    public void setTipoImagen(String tipoImagen) {
+        this.tipoImagen = tipoImagen;
+    }
+
+    public int getEnviado() {
+        return enviado;
+    }
+
+    public void setEnviado(int enviado) {
+        this.enviado = enviado;
+    }
+
+    public int getCertificadoId() {
+        return certificadoId;
+    }
+
+    public void setCertificadoId(int certificadoId) {
+        this.certificadoId = certificadoId;
+    }
+}
